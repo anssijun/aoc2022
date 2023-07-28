3a75d87027565b69d11ac235c6c4c5b4f9154e82
anssijun <anssi.junnola@gmail.com>
2023-07-28 20:21:17 +0100

    Day 1, part 2

--- day1.py
+++ day1.py
@@ -1,7 +1,10 @@
 # https://adventofcode.com/2022/day/1
-def calc_max_calories():
+import heapq
+
+
+def calc_max_calories(top_n=1):
     # Max score
-    max_calories = 0
+    max_calories = [0] * top_n
     # Single elf calories
     calories = 0
     with open('inputs/day1') as f:
@@ -11,12 +14,14 @@ def calc_max_calories():
                 calories += int(line.strip())
             except ValueError:
                 # Empty line denotes a new elf - compare max scores and move to the next elf
-                if calories > max_calories:
-                    max_calories = calories
+                # Handle max_calories as a min heap for easily replacing the smallest "top score" with a new one
+                if calories > max_calories[0]:
+                    heapq.heappushpop(max_calories, calories)
                 calories = 0
 
-    print('Max calories:', max_calories)
+    print('Max calories:', sum(max_calories))
 
 
 if __name__ == '__main__':
     calc_max_calories()
+    calc_max_calories(top_n=3)
