495cb3f70907c009527c56004a05a77b357ca545
anssijun <anssi.junnola@gmail.com>
2023-07-29 15:39:53 +0100

    Day 5

---
+++ day5.py
@@ -0,0 +1,33 @@
+# https://adventofcode.com/2022/day/5
+import re
+from collections import defaultdict, deque
+
+
+if __name__ == '__main__':
+    # Stacks is a dict of deques (could be list of deques but this is easier to build when reading through input)
+    # Deques are used because you can appendleft, so it's more natural from building the stack top to bottom
+    stacks = defaultdict(deque)
+    with open('inputs/day5') as f:
+        for line in f:
+            line = line.rstrip()
+            sl = line.lstrip()
+            if sl.startswith('['):
+                stack_counter = 0
+                for i, v in enumerate(line):
+                    if v.isalnum():
+                        # We know square brackets denote a crate, and we're interested in the letter inside. There's a
+                        # space between crates, so we can use the index to calculate which stack the number belongs to:
+                        # first stack would be index 1, second stack would be index 5, this stack index 9 etc
+                        stacks[((i-1)//4)+1].appendleft(v)
+                        stack_counter += 1
+            elif sl.startswith('move'):
+                count, source, target = re.findall(r'\d+', sl)
+                count, source, target = int(count), int(source), int(target)
+                crates = []
+                for i in range(count):
+                    crates.append(stacks[source].pop())
+                stacks[target].extend(crates)
+
+    for i in sorted(stacks.keys()):
+        print(stacks[i][-1], end='')
+    print()
