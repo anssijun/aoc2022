6e7c2ea66c3ffd46163cef138d9df9ccf261ee46
anssijun <anssi.junnola@gmail.com>
2023-07-29 12:04:34 +0100

    Day 4, part 2

--- day4.py
+++ day4.py
@@ -1,6 +1,11 @@
 # https://adventofcode.com/2022/day/4
 
-if __name__ == '__main__':
+def overlap(minx, maxx, miny, maxy, partially=True):
+    if partially:
+        return (maxx >= miny and maxy >= minx) or (maxy >= minx and maxx >= miny)
+    return (minx <= miny and maxx >= maxy) or (miny <= minx and maxy >= maxx)
+
+def calc_count(part2=False):
     c = 0
     with open('inputs/day4') as f:
         for line in f:
@@ -8,6 +13,11 @@ if __name__ == '__main__':
             minx, maxx = pairs[0].split('-')
             miny, maxy = pairs[1].split('-')
             minx, miny, maxx, maxy = int(minx), int(miny), int(maxx), int(maxy)
-            if (minx <= miny and maxx >= maxy) or (miny <= minx and maxy >= maxx):
+            if overlap(minx, maxx, miny, maxy, part2):
                 c += 1
-    print(c)
+    return c
+
+
+if __name__ == '__main__':
+    print(calc_count())
+    print(calc_count(True))
