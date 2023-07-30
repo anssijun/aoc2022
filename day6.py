0b9f3f5e902b45e96655d1782a602041f61a8c29
anssijun <anssi.junnola@gmail.com>
2023-07-30 12:57:42 +0100

    Day 6, part 2

--- day6.py
+++ day6.py
@@ -2,19 +2,22 @@
 from collections import deque
 
 
-if __name__ == '__main__':
-    header_len = 4
+def unique_string_end_idx(length):
     counter = 0
     buffer = deque()
-    char_to_idx = {}
     with open('inputs/day6') as f:
         # Read the file a character at a time (IRL we'd read more at a time to a buffer)
         while c := f.read(1):
             counter += 1
             buffer.append(c)
-            if len(buffer) == header_len:
+            if len(buffer) == length:
                 # If the deque is full, check if all characters are unique by casting to set
-                if len(set(buffer)) == header_len:
+                if len(set(buffer)) == length:
                     break
                 buffer.popleft()
-    print(counter)
+    return counter
+
+
+if __name__ == '__main__':
+    print(unique_string_end_idx(4))
+    print(unique_string_end_idx(14))
