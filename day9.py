5737340f8231868f0660f036a4ca924d18f39d2c
anssijun <anssi.junnola@gmail.com>
2023-08-29 19:38:40 +0100

    Day 9, part 2

--- day9.py
+++ day9.py
@@ -1,43 +1,55 @@
 # https://adventofcode.com/2022/day/9
 
-def move(direction, distance, moves, head, tail):
+def move(direction, distance, moves, knots, rope_length):
     for i in range(distance):
-        # Move the head
         match direction:
+            # Move the head
             case 'L':
-                head[0] -= 1
+                knots[0][0] -= 1
             case 'U':
-                head[1] += 1
+                knots[0][1] += 1
             case 'R':
-                head[0] += 1
+                knots[0][0] += 1
             case 'D':
-                head[1] -= 1
+                knots[0][1] -= 1
             case default:
                 pass
 
-        # Figure out whether the tail needs to move
-        need_to_move_horizontally = abs(tail[0] - head[0]) > 1
-        need_to_move_vertically = abs(tail[1] - head[1]) > 1
+        # After the head has been moved, move the rest of the rope
+        for j in range(1, rope_length):
+            # Figure out how the current knot has to move compared to the previous knot
+            need_to_move_horizontally = abs(knots[j][0] - knots[j-1][0]) > 1
+            need_to_move_vertically = abs(knots[j][1] - knots[j-1][1]) > 1
 
-        if need_to_move_horizontally:
-            tail[0] += 1 if head[0] > tail[0] else -1
-            # Do we also need to move diagonally?
-            if head[1] != tail[1]:
-                tail[1] += 1 if head[1] > tail[1] else -1
-        elif need_to_move_vertically:
-            tail[1] += 1 if head[1] > tail[1] else -1
-            if head[0] != tail[0]:
-                tail[0] += 1 if head[0] > tail[0] else -1
-        # Add the tail position to moves set (tuple is immutable so can be used as key)
-        moves.add(tuple(tail))
+            if need_to_move_horizontally:
+                knots[j][0] += 1 if knots[j-1][0] > knots[j][0] else -1
+                # Do we also need to move diagonally?
+                if knots[j-1][1] != knots[j][1]:
+                    knots[j][1] += 1 if knots[j-1][1] > knots[j][1] else -1
+            elif need_to_move_vertically:
+                knots[j][1] += 1 if knots[j-1][1] > knots[j][1] else -1
+                if knots[j-1][0] != knots[j][0]:
+                    knots[j][0] += 1 if knots[j-1][0] > knots[j][0] else -1
+
+            # Only interested in tail positions
+            if j == rope_length - 1:
+                # Add the tail position to moves set (tuple is immutable so can be used as key)
+                moves.add((knots[j][0], knots[j][1]))
+
+
+def calculate_position(cmds, rope_length=2):
+    moves = set()
+    knots = []
+    for i in range(rope_length):
+        knots.append([0, 0])
+    for cmd in cmds:
+        cmd = cmd.split()
+        move(cmd[0], int(cmd[1]), moves, knots, rope_length=rope_length)
+    return len(moves)
 
 
 if __name__ == '__main__':
-    moves = set()
-    head = [0, 0]
-    tail = [0, 0]
     with open('inputs/day9') as f:
-        for line in f.readlines():
-            cmd = line.split()
-            move(cmd[0], int(cmd[1]), moves, head, tail)
-    print(len(moves))
+        cmds = f.readlines()
+    print(calculate_position(cmds))
+    print(calculate_position(cmds, rope_length=10))
