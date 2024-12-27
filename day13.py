9deaee1ce9244555981e1c9bd685e2b05c6446f2
anssijun <anssi.junnola@gmail.com>
2024-12-27 15:42:01 -0800

    Day 13, part 2

--- day13.py
+++ day13.py
@@ -1,11 +1,13 @@
 # https://adventofcode.com/2022/day/13
 import ast
+import math
+from functools import cmp_to_key
 from typing import Union
 
 
-def compare_packet_pair(pair: list[Union[int, list]]):
+def compare_packet_pair(left: Union[int, list], right: Union[int, list]):
     c = 0
-    result = None
+    result = 0
     # Loop through the lists in the pair, compare items and figure out whether they're in the correct order.
     # 1. First checks that both lists still have entries. If one has run out of entries (but the other has not),
     #    we have a result.
@@ -13,39 +15,39 @@ def compare_packet_pair(pair: list[Union[int, list]]):
     # 2.1. If both are integers, compare them. If they're not equal, we have a result.
     # 2.2. If either side is a list and the other is not, call this function recursively and wrap the other side in a list.
     #      This might mean that we recurse multiple times before starting to check the values.
-    while result is None:
+    while result == 0:
         try:
-            left = pair[0][c]
+            left_value = left[c]
         except IndexError:
-            left = None
+            left_value = None
         try:
-            right = pair[1][c]
+            right_value = right[c]
         except IndexError:
-            right = None
+            right_value = None
 
-        print(f'Compare {left} and {right}')
+        print(f'Compare {left_value} and {right_value}')
 
-        if left is None and right is not None:
+        if left_value is None and right_value is not None:
             # If left hand side runs out first, inputs are in the right order
-            return True
-        elif left is not None and right is None:
+            return 1
+        elif left_value is not None and right_value is None:
             # If right runs out first, inputs are not in the right order
-            return False
-        elif left is None and right is None:
+            return -1
+        elif left_value is None and right_value is None:
             # Both left and right ran out so it's inconclusive - move on to next entries
-            return
+            return 0
 
         # Compare elements. If both are ints, they can be compared directly. If either side is a list and the other one
         # and integer, convert the integer to list and call this function recursively. If both are lists, also call this
         # function recursively.
-        if isinstance(left, int) and isinstance(right, int):
-            result = compare_ints(left, right)
-        elif isinstance(left, list) and isinstance(right, list):
-            result = compare_packet_pair([left, right])
-        elif isinstance(left, list):
-            result = compare_packet_pair([left, [right]])
-        elif isinstance(right, list):
-            result = compare_packet_pair([[left], right])
+        if isinstance(left_value, int) and isinstance(right_value, int):
+            result = compare_ints(left_value, right_value)
+        elif isinstance(left_value, list) and isinstance(right_value, list):
+            result = compare_packet_pair(left_value, right_value)
+        elif isinstance(left_value, list):
+            result = compare_packet_pair(left_value, [right_value])
+        elif isinstance(right_value, list):
+            result = compare_packet_pair([left_value], right_value)
 
         # Break to return a result only if we have one, otherwise check next elements
         if result:
@@ -58,9 +60,10 @@ def compare_packet_pair(pair: list[Union[int, list]]):
 
 def compare_ints(left: int, right: int):
     if left > right:
-        return False
+        return -1
     elif left < right:
-        return True
+        return 1
+    return 0
 
 
 def parse_input(filename: str):
@@ -70,10 +73,8 @@ def parse_input(filename: str):
         c = 0
         for line in f.readlines():
             c += 1
-            if c == 1:
-                packets.append([parse_line(line)])
-            elif c == 2:
-                packets[-1].append(parse_line(line))
+            if c in (1, 2):
+                packets.append(parse_line(line))
             else:
                 c = 0
     return packets
@@ -87,11 +88,26 @@ def parse_line(line: str):
 def main():
     packets = parse_input('inputs/day13')
     correct_packets = []
-    for idx, packet in enumerate(packets):
-        if compare_packet_pair(packet):
-            correct_packets.append(idx + 1)
+    # Loop through the list in steps of two, in order to compare packets
+    for i in range(0, len(packets), 2):
+        if compare_packet_pair(packets[i], packets[i+1]) == 1:
+            # We're interested in the indices of package pairs, thus we need some simple math here
+            correct_packets.append(math.ceil((i + 1) / 2))
         print()
-    print(sum(correct_packets))
+    print('Sum of correct package\'s indices is: ', sum(correct_packets))
+
+    # For part two, we need to add a couple of "divider" packets, sort the list and get a product of the divider packets'
+    # indices (1-based). Simple enough as we can use the compare_packet_pair -function as the sort key. (Python's sort
+    # natively takes only one argument, but luckily we can wrap the key function to cmp_to_key to support two arguments.)
+    packets.append([[2]])
+    packets.append([[6]])
+
+    packets.sort(key=cmp_to_key(compare_packet_pair), reverse=True)
+
+    two_index = packets.index([[2]]) + 1
+    six_index = packets.index([[6]]) + 1
+
+    print('The product of [[2]] and [[6]] indices is: ', two_index * six_index)
 
 
 if __name__ == '__main__':
