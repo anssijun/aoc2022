c9bbc2fafda87d650ea08ccb29073aa3c6e6e94b
anssijun <anssi.junnola@gmail.com>
2024-12-29 10:50:23 -0800

    Day 14, part 2

--- day14.py
+++ day14.py
@@ -1,31 +1,63 @@
-def simulate_sand(cave: list[list[str]], source_coords: tuple[int, int]):
-    # Simulate the falling sand by finding the landing spot for each unit of sand and counting them. Once an IndexError
-    # is raised, we know that the sand will start falling into the abyss and can stop the simulation.
+import copy
+
+
+def simulate_sand(cave: list[list[str]], source_coords: tuple[int, int], fill_to_source: bool=False):
+    # Simulate the falling sand by finding the landing spot for each unit of sand and counting them.
+    # If parameter fill_to_source is true, once an IndexError is raised, we know that the sand will start falling into
+    # the abyss and can stop the simulation.
+    # If fill_to_source is false, we know there's a floor beneath, and we dynamically expand the cave horizontally until
+    # the source is blocked.
+    cave = copy.deepcopy(cave)
     counter = 0
     while True:
         try:
             landing_coords = find_landing_coords(cave, source_coords)
         except IndexError:
+            if fill_to_source:
+                # If sand starts falling out of bounds of the cave, it is dynamically extended horizontally to both
+                # directions, and the same unit of sand is retried. This is rather ugly and slow, probably indicating
+                # that a list of lists was the wrong data structure choice for this problem. However, it does work,
+                # potentially infinitely (or at least until we start running out of memory).
+                cave = extend_cave(cave)
+                source_coords = source_coords[0] + 1, source_coords[1]
+                continue
             break
+
         cave[landing_coords[1]][landing_coords[0]] = 'o'
         counter += 1
         # for i in cave: print(''.join(i))
         # print()
+
+        if landing_coords == source_coords:
+            break
     return counter
 
 
 def find_landing_coords(cave: list[list[str]], current_coords: tuple[int, int]):
     # Finds the landing coords for the sand by recursively searching first down, the down and left and finally down and
     # right. Valid landing coords are have a unit of air '.' in them but either rock or sand below, including to the
-    # left and right. If the sand would fall out of the bounds of the cave, an index error is raised as this is the
+    # left and right. If the sand falls out of the bounds of the cave, an index error is raised as this is the
     # terminal state for the algorithm (could/should be some other error or mechanism but whatever).
     if cave[current_coords[1] + 1][current_coords[0]] != '.':
         if cave[current_coords[1] + 1][current_coords[0] - 1] != '.':
+            # Raise IndexError manually if we try to get out of bounds using negative indices (as in Python these just
+            # access list items from the end of the list)
+            if current_coords[0] - 1 < 0:
+                raise IndexError
             if cave[current_coords[1] + 1][current_coords[0] + 1] != '.':
                 return current_coords
             return find_landing_coords(cave, (current_coords[0] + 1, current_coords[1] + 1))
         return find_landing_coords(cave, (current_coords[0] - 1, current_coords[1] + 1))
     return find_landing_coords(cave, (current_coords[0], current_coords[1] + 1))
+
+
+def extend_cave(cave: list[list[str]]):
+    for i in range(len(cave) - 1):
+        cave[i].insert(0, '.')
+        cave[i].append('.')
+    cave[-1].insert(0, '#')
+    cave[-1].append('#')
+    return cave
 
 
 def build_cave(data: list[str]):
@@ -87,6 +119,10 @@ def main():
     max_sand = simulate_sand(cave, (500 - x_offset, 0))
     print(f'{max_sand} unit of sand can fall before it starts to flow into the abyss')
 
+    cave.append(['.' for _ in cave[-1]])
+    cave.append(['#' for _ in cave[-1]])
+    max_sand = simulate_sand(cave, (500 - x_offset, 0), True)
+    print(f'{max_sand} unit of sand can fall before the source becomes blocked')
 
 
 if __name__ == '__main__':
